3bb443470a93a528489d743964248d7fd4be0817
lelouch <lilnobp5@gmail.com>
2022-05-11 13:01:17 +0200

    create new playerService interface

---
+++ src/main/java/de/htwberlin/service/PlayerService.java
@@ -0,0 +1,12 @@
+package de.htwberlin.service;
+
+public interface PlayerService {
+
+    /**
+     * create a new player
+     *
+     * @param name of the Player
+     * @return Player as a Object
+     */
+    Player createPlayer(String name);
+}
