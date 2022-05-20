2b5758e173e8c218ab50d5e3d827b8faaf955031
islamabdelrahman <islam.abdelrahman00@gmail.com>
2022-05-20 17:10:21 +0200

    added 2 tests for checkIfCardHasGameRule method

--- CardManagement/src/test/java/de/htwberlin/service/CardServiceTest.java
+++ CardManagement/src/test/java/de/htwberlin/service/CardServiceTest.java
@@ -1,38 +1,36 @@
 package de.htwberlin.service;
 
 import de.htwberlin.entity.Card;
+import de.htwberlin.entity.GameRule;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CardServiceTest {
 
     CardService cardService;
 
     @Before
-    public void initializeCardService(){
+    public void initializeCardService() {
         cardService = new CardServiceImpl();
     }
 
     @Test
-    public void getCardAsString_testInitializedCard(){
+    public void getCardAsString_testInitializedCard() {
         //given
-        Card nullCard = new Card(Card.Rank.ACE, Card.Symbol.DIAMONDS);
+        Card card = new Card(Card.Rank.ACE, Card.Symbol.DIAMONDS);
 
         //when
-        String result = cardService.getCardAsString(nullCard);
+        String result = cardService.getCardAsString(card);
 
         //then
         assertEquals(result, "ACE DIAMONDS");
     }
 
     @Test
-    public void getCardAsString_testNullCard(){
+    public void getCardAsString_testNullCard() {
         //given
         Card nullCard = new Card(null, null);
 
@@ -43,6 +41,31 @@ public class CardServiceTest {
         assertTrue(!result.isEmpty());
     }
 
+    @Test
+    public void checkIfCardHasGameRule_testNonRuleCard() {
+        //given
+        Card normalCard = new Card(Card.Rank.KING, Card.Symbol.CLUBS);
+
+        //when
+        GameRule rule = cardService.checkIfCardHasGameRule(normalCard);
+
+        //then
+        assertEquals(GameRule.NONE, rule);
+    }
+
+    @Test
+    public void checkIfCardHasGameRule_testRuleCard() {
+        //given
+        Card ruleCard = new Card(Card.Rank.SEVEN, Card.Symbol.CLUBS);
+
+        //when
+        GameRule rule = cardService.checkIfCardHasGameRule(ruleCard);
+
+        //then
+        assertEquals(GameRule.NEXT_PLAYER_DRAWS_CARDS, rule);
+
+
+    }
 
 
 }
